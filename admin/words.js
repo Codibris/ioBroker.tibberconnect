a636d93d7def380a01cf854a9f5e2940d8fb1cce
Rumen <rumen.schmidt@gmx.de>
2022-02-18 18:14:05 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"tibberconnect adapter settings": {
+		en: "Adapter settings for tibberconnect",
+		de: "Adaptereinstellungen für tibberconnect",
+		ru: "Настройки адаптера для tibberconnect",
+		pt: "Configurações do adaptador para tibberconnect",
+		nl: "Adapterinstellingen voor tibberconnect",
+		fr: "Paramètres d'adaptateur pour tibberconnect",
+		it: "Impostazioni dell'adattatore per tibberconnect",
+		es: "Ajustes del adaptador para tibberconnect",
+		pl: "Ustawienia adaptera dla tibberconnect",
+		"zh-cn": "tibberconnect的适配器设置",
+	},
+	option1: {
+		en: "option1",
+		de: "Option 1",
+		ru: "Опция 1",
+		pt: "Opção 1",
+		nl: "Optie 1",
+		fr: "Option 1",
+		it: "opzione 1",
+		es: "Opción 1",
+		pl: "opcja 1",
+		"zh-cn": "选项1",
+	},
+	option2: {
+		en: "option2",
+		de: "Option 2",
+		ru: "вариант 2",
+		pt: "opção 2",
+		nl: "Optie 2",
+		fr: "Option 2",
+		it: "opzione 2",
+		es: "opcion 2",
+		pl: "Opcja 2",
+		"zh-cn": "选项2",
+	},
+};
---
+++ build/main.js
@@ -0,0 +1,89 @@
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __defProps = Object.defineProperties;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropDescs = Object.getOwnPropertyDescriptors;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getOwnPropSymbols = Object.getOwnPropertySymbols;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __propIsEnum = Object.prototype.propertyIsEnumerable;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __spreadValues = (a, b) => {
+  for (var prop in b || (b = {}))
+    if (__hasOwnProp.call(b, prop))
+      __defNormalProp(a, prop, b[prop]);
+  if (__getOwnPropSymbols)
+    for (var prop of __getOwnPropSymbols(b)) {
+      if (__propIsEnum.call(b, prop))
+        __defNormalProp(a, prop, b[prop]);
+    }
+  return a;
+};
+var __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b));
+var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
+var __reExport = (target, module2, copyDefault, desc) => {
+  if (module2 && typeof module2 === "object" || typeof module2 === "function") {
+    for (let key of __getOwnPropNames(module2))
+      if (!__hasOwnProp.call(target, key) && (copyDefault || key !== "default"))
+        __defProp(target, key, { get: () => module2[key], enumerable: !(desc = __getOwnPropDesc(module2, key)) || desc.enumerable });
+  }
+  return target;
+};
+var __toESM = (module2, isNodeMode) => {
+  return __reExport(__markAsModule(__defProp(module2 != null ? __create(__getProtoOf(module2)) : {}, "default", !isNodeMode && module2 && module2.__esModule ? { get: () => module2.default, enumerable: true } : { value: module2, enumerable: true })), module2);
+};
+var utils = __toESM(require("@iobroker/adapter-core"));
+class Tibberconnect extends utils.Adapter {
+  constructor(options = {}) {
+    super(__spreadProps(__spreadValues({}, options), {
+      name: "tibberconnect"
+    }));
+    this.on("ready", this.onReady.bind(this));
+    this.on("stateChange", this.onStateChange.bind(this));
+    this.on("unload", this.onUnload.bind(this));
+  }
+  async onReady() {
+    this.log.info("config option1: " + this.config.option1);
+    this.log.info("config option2: " + this.config.option2);
+    await this.setObjectNotExistsAsync("testVariable", {
+      type: "state",
+      common: {
+        name: "testVariable",
+        type: "boolean",
+        role: "indicator",
+        read: true,
+        write: true
+      },
+      native: {}
+    });
+    this.subscribeStates("testVariable");
+    await this.setStateAsync("testVariable", true);
+    await this.setStateAsync("testVariable", { val: true, ack: true });
+    await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
+    let result = await this.checkPasswordAsync("admin", "iobroker");
+    this.log.info("check user admin pw iobroker: " + result);
+    result = await this.checkGroupAsync("admin", "admin");
+    this.log.info("check group user admin group admin: " + result);
+  }
+  onUnload(callback) {
+    try {
+      callback();
+    } catch (e) {
+      callback();
+    }
+  }
+  onStateChange(id, state) {
+    if (state) {
+      this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+    } else {
+      this.log.info(`state ${id} deleted`);
+    }
+  }
+}
+if (require.main !== module) {
+  module.exports = (options) => new Tibberconnect(options);
+} else {
+  (() => new Tibberconnect())();
+}
+//# sourceMappingURL=main.js.map
